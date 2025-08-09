9d64904982814ad3af6e9842fd7a9cb97abf0e27
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-09 17:04:31 +0200

    Added a method to change application theme at runtime.

--- LibMicroDesk/MDCore.cs
+++ LibMicroDesk/MDCore.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using LibMicroDesk.Windows;
 
 namespace LibMicroDesk
@@ -13,6 +14,37 @@ namespace LibMicroDesk
         public static void AboutBox()
         {
             _ = new WndAbout().ShowDialog();
+        }
+
+        /// <summary>
+        /// Applies the specified system theme to the application.
+        /// </summary>
+        /// <param name="theme">New theme to be applied.</param>
+        public static void ApplyTheme(SystemTheme theme)
+        {
+            #pragma warning disable WPF0001
+
+            switch (theme)
+            {
+                default:
+                case SystemTheme.None:
+                Application.Current.ThemeMode = ThemeMode.None;
+                break;
+
+            case SystemTheme.Light:
+                Application.Current.ThemeMode = ThemeMode.Light;
+                break;
+
+            case SystemTheme.Dark:
+                Application.Current.ThemeMode = ThemeMode.Dark;
+                break;
+
+            case SystemTheme.Auto:
+                Application.Current.ThemeMode = ThemeMode.System;
+                break;
+            }
+
+            #pragma warning restore WPF0001
         }
     }
 }
---
+++ LibMicroDesk/SystemTheme.cs
@@ -0,0 +1,27 @@
+﻿namespace LibMicroDesk;
+
+/// <summary>
+/// Representing the system theme enumeration.
+/// </summary>
+public enum SystemTheme
+{
+    /// <summary>
+    /// No custom theme is applied.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Ligt fluent theme is applied.
+    /// </summary>
+    Light,
+
+    /// <summary>
+    /// Dark fluent theme is applied.
+    /// </summary>
+    Dark,
+
+    /// <summary>
+    /// System theme is applied, which follows the user's system settings.
+    /// </summary>
+    Auto
+}
