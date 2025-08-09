8991a4dd15d702047fcd47e07cab987acdca1d5c
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-09 15:50:23 +0200

    Added a common functions library: LibMicroDesk

---
+++ LibMicroDesk/MDCore.cs
@@ -0,0 +1,10 @@
+
+namespace LibMicroDesk
+{
+    /// <summary>
+    /// Representing the core functionality of the MicroDesk library.
+    /// </summary>
+    public static class MDCore
+    {
+    }
+}
