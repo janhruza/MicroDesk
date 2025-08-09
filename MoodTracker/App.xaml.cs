dc13fca44c211e2afa3e09f38b1e00cc82201bc9
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-09 13:27:19 +0200

    Added a mood page.

---
+++ MoodTracker/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using MoodTracker.Pages;
+
+namespace MoodTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MoodTracker.MainWindow mw = new MoodTracker.MainWindow();
+            MainWindow = mw;
+            MainWindow.Show();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            return;
+        }
+
+        #region Pages
+
+        /// <summary>
+        /// Representing the mood page instance.
+        /// </summary>
+        internal static PgMood MoodPage => new PgMood();
+
+        #endregion
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# MicroDesk
+# Micro Desktop
+
+## About Micro Desktop
+Micro Desktop is a solution full of small tools to help you with your daily tasks. It is designed to be lightweight and efficient, providing a clean and simple interface for quick access to various functionalities.
+
+Applications included in Micro Desktop are designed to be minimalistic and to serve only one purpose. Some of them can be more useful then others.
+
+The goal is to learn and have fun coding!
