ace03caf984e048332a9f4351fd2e697cf135d25
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-12 18:27:39 +0200

    Added a new project: QuickNotes

---
+++ QuickNotes/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace QuickNotes;
+
+/// <summary>
+/// Representing the main application class.
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        MainWindow mw = new MainWindow();
+        MainWindow = mw;
+        MainWindow.Show();
+    }
+
+    private void Application_Exit(object sender, ExitEventArgs e)
+    {
+        return;
+    }
+}
---
+++ QuickNotes/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using LibMicroDesk.Windows;
+
+namespace QuickNotes;
+
+/// <summary>
+/// Representing the main application window.
+/// </summary>
+public partial class MainWindow : IconlessWindow
+{
+    /// <summary>
+    /// Creates a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
