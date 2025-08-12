1502032f7ce0c3a3757ada37ffccb66c24ae548e
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-12 19:27:53 +0200

    Added logic for the note removal.

--- QuickNotes/App.xaml.cs
+++ QuickNotes/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using QuickNotes.Core;
 using QuickNotes.Pages;
+using System;
 
 namespace QuickNotes;
 
@@ -8,12 +11,36 @@ namespace QuickNotes;
 /// </summary>
 public partial class App : Application
 {
+    static App()
+    {
+        Notes = [];
+        GenerateTestData();
+    }
+
     #region Page definitions
 
     /// <summary>
     /// Representing the overview page.
     /// </summary>
     public static PgOverview OverviewPage => new PgOverview();
+
+    #endregion
+
+    #region Static code
+
+    private static void GenerateTestData()
+    {
+        Notes.AddRange([
+            new Note("Create a new application.", DateTime.Now, DateTime.Now.AddDays(7)),
+            new Note("Go to a barber shop.", DateTime.Now, DateTime.Now.AddDays(14)),
+            new Note("Feed cat.", DateTime.Now, DateTime.Now.AddDays(-3)),
+        ]);
+    }
+
+    /// <summary>
+    /// Representing a list of all loaded notes.
+    /// </summary>
+    public static List<Note> Notes { get; }
 
     #endregion
 
--- QuickNotes/Pages/PgOverview.xaml.cs
+++ QuickNotes/Pages/PgOverview.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using QuickNotes.Core;
 
 namespace QuickNotes.Pages;
 
@@ -14,10 +15,103 @@ public partial class PgOverview : Page
     public PgOverview()
     {
         InitializeComponent();
+        this.RefreshUI();
+    }
+
+    private Button CreateDeleteButton(Note? target)
+    {
+        Button btn = new Button
+        {
+            Content = "",   // delete character
+            FontFamily = new System.Windows.Media.FontFamily("Segoe Fluent Icons"),
+            Visibility = System.Windows.Visibility.Collapsed,
+            Margin = new Thickness(5, 0, 0, 0)
+        };
+
+        btn.Click += delegate
+        {
+            // delete the note
+            if (target.HasValue == false)
+            {
+                return;
+            }
+
+            var note = target.Value;
+            if (MessageBox.Show($"Do you want to delete the \'{note.Text}\' note? This action is irreversible.", "Delete note?", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                // remove the note from the notes list
+                if (App.Notes.Remove(note) == false)
+                {
+                    // can't remove note
+                    _ = MessageBox.Show("Unable to remove the selected note. Please try again later.", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                RefreshUI();
+                return;
+            }
+        };
+
+        return btn;
+    }
+
+    private void RefreshUI()
+    {
+        // load all notes and display them
+        // into the list box
+        lbxNotes.Items.Clear();
+
+        if (App.Notes.Count == 0)
+        {
+            // no need to refresh the items list
+            return;
+        }
+
+        foreach (Note note in App.Notes)
+        {
+            // create item's content
+            Button btnDelete = CreateDeleteButton(note);
+
+            ListBoxItem lbiNote = new ListBoxItem
+            {
+                HorizontalContentAlignment = HorizontalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                Content = new Grid
+                {
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition(),
+                        new ColumnDefinition { Width = GridLength.Auto }
+                    },
+
+                    Children =
+                    {
+                        new Label
+                        {
+                            Content = note.Text,
+                            VerticalAlignment = VerticalAlignment.Center
+                        },
+
+                        btnDelete
+                    }
+                }
+            };
+
+            Grid.SetColumn(btnDelete, 1);
+
+            lbiNote.MouseEnter += (s, e) => btnDelete.Visibility = Visibility.Visible;
+            lbiNote.MouseLeave += (s, e) => btnDelete.Visibility = Visibility.Collapsed;
+
+            lbiNote.Tag = note;
+            lbxNotes.Items.Add(lbiNote);
+        }
+
+        return;
     }
 
     private void btnRefresh_Click(object sender, RoutedEventArgs e)
     {
+        this.RefreshUI();
         return;
     }
 
