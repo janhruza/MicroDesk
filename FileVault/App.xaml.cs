f2fffa755d09480e35b012342af79b2871361749
Jan Hrůza <honza.hruza.d5@gmail.com>
2025-08-25 20:52:05 +0200

    Added a new project: FileVault

---
+++ FileVault/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace FileVault
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // app startup
+            MainWindow mw = new MainWindow();
+            MainWindow = mw;
+            MainWindow.Show();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            // post exit cleanup
+            return;
+        }
+    }
+}
---
+++ FileVault/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using LibMicroDesk.Windows;
+
+namespace FileVault;
+
+/// <summary>
+/// Representing the main window class.
+/// </summary>
+public partial class MainWindow : IconlessWindow
+{
+    /// <summary>
+    /// Creates a new <see cref="MainWindow"/> instance.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
