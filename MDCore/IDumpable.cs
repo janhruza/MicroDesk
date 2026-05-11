80a95a2175f6619446da28754b6a38f98af18e04
Jan Hrůza <honza.hruza.d5@hotmail.com>
2026-05-12 00:36:32 +0200

    Added new IDumpable interface.

---
+++ MDCore/IDumpable.cs
@@ -0,0 +1,21 @@
+﻿namespace MDCore;
+
+/// <summary>
+/// Defines methods to serialize and deserialize objects of type T to and from a file.
+/// </summary>
+public interface IDumpable
+{
+    /// <summary>
+    /// Writes the specified data to a file with the given name.
+    /// </summary>
+    /// <param name="fileName">The path and name of the file to which the data will be written. Cannot be null or empty.</param>
+    /// <returns>true if the data was successfully written to the file; otherwise, false.</returns>
+    bool WriteToFile(string fileName);
+
+    /// <summary>
+    /// Attempts to read and deserialize data of type T from the specified file.
+    /// </summary>
+    /// <param name="fileName">The path to the file to read. Cannot be null or empty.</param>
+    /// <returns>true if the file was read and deserialized successfully; otherwise, false.</returns>
+    bool ReadFromFile(string fileName);
+}
--- Update/Core/Settings.cs
+++ Update/Core/Settings.cs
@@ -1,17 +1,40 @@
-﻿namespace Update.Core;
+﻿using MDCore;
+
+namespace Update.Core;
 
 /// <summary>
 /// Representing the basic app settings class.
 /// </summary>
-public class Settings
+public class Settings : IDumpable
 {
     /// <summary>
     /// Creates a new empty instance of the <see cref="Settings"/> class. This constructor is used to create a new settings object with default values.
     /// </summary>
     public Settings()
     {
-        
+        Log.Info("Settings instance has been created.");
+        return;
     }
 
-    
+    /// <summary>
+    /// Destroys the instance of the <see cref="Settings"/> class. This destructor is used to clean up any resources used by the settings object.
+    /// </summary>
+    ~Settings()
+    {
+        Log.Info("Settings instance is being destroyed.");
+    }
+
+    /// <inheritdoc/>
+    public bool ReadFromFile(string fileName)
+    {
+        // TODO : Implement the method to read the settings from a file.
+        return false;
+    }
+
+    /// <inheritdoc/>
+    public bool WriteToFile(string fileName)
+    {
+        // TODO : Implement the method to write the settings to a file.
+        return false;
+    }
 }
